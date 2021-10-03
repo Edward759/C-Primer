46b760e189d39784d6acee88331e3cc83df9bf05
Edward759 <759263934@qq.com>
2021-10-03 10:30:15 +0800

    #update ch3

--- ch3.h
+++ ch3.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 void exe_2_1()
@@ -88,4 +89,67 @@ void exe_10()
         if(ispunct(*iter))
             str.erase(iter);
     cout << str << endl;
+}
+
+void exe_21()
+{
+    vector<int> v = {1, 2, 3, 4, 5};
+    for (auto it = v.cbegin(); it != v.cend();it++)
+        cout << *it << endl;
+}
+
+void exe_22()
+{
+    string str;
+    cin >> str;
+    for (auto it = str.begin(); it != str.end();++it)
+        *it = toupper(*it);
+    cout << str << endl;
+}
+
+void exe_23()
+{
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (auto it = v.begin(); it != v.end();it++)
+        *it = 2 * (*it);
+    for(auto i:v)
+        cout << i << ' ';
+    cout << endl;
+}
+
+void exe_24()
+{
+    int i;
+    vector<int> v;
+
+    while(cin>>i)
+        v.push_back(i);
+
+    for(auto it = v.begin() + 1; it != v.end();it++)
+        cout << (*it) - *(it - 1) << ' ';
+    cout << endl;
+
+    auto l = v.begin(), r = v.end() - 1;
+    while(l!=r)
+    {
+        cout << (*l) - (*r) << ' ';
+        l++;
+        r--;
+    }
+    cout << endl;
+}
+
+void exe_25()
+{
+    vector<unsigned> scores(11, 0);
+    unsigned grade;
+    auto it = scores.begin();
+    while (cin >> grade)
+    {
+        if(grade <= 100)
+            ++(*(it + grade / 10));
+    }
+    for(auto i:scores)
+        cout << i << ' ';
+    cout << endl;
 }
